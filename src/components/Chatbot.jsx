275fc1bf45aebcd3aa6d2828b7454c99813d264a
nespinoza2000 <nespinozasegovia@gmail.com>
2024-07-20 14:34:09 -0400

    scrollbar añadido

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,23 +1,6 @@
-import React, { useState } from "react";
-import openaiservice from "../services/openai.js";
+import React from 'react';
 
-const Chatbot = ({ fetchPlaces }) => {
-  const [messages, setMessages] = useState([]); // Example { role: "system", content: "Hola, ¿qué lugares turisticos de Encarnación te gustaria visitar?" }, { role: "user", content: "Recomiendame lugares bailables." }, { role: "system", content: "Claro, estos son los..." }
-  const [message, setMessage] = useState("");
-
-  const handleRequest = (message) => {
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { role: "user", content: message },
-    ]);
-    // Call OpenAI API
-    fetchPlaces(message);
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { role: "system", content: "Claro, te he añadido los puntos en el mapa" },
-    ]);
-  };
-
+const Chatbot = () => {
   return (
     <div>
       <button
@@ -39,111 +22,119 @@ const Chatbot = ({ fetchPlaces }) => {
           strokeLinejoin="round"
           className="text-white block border-gray-200 align-middle"
         >
-          <path
-            d="m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z"
-            className="border-gray-200"
-          ></path>
+          <path d="m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z" className="border-gray-200" />
         </svg>
       </button>
 
       <div
-        style={{
-          boxShadow: "0 0 #0000, 0 0 #0000, 0 1px 2px 0 rgb(0 0 0 / 0.05)",
-        }}
-        className="fixed bottom-[calc(4rem+1.5rem)] right-0 mr-4 bg-white p-6 rounded-lg border border-[#e5e7eb] w-[440px] h-[634px]"
+        style={{ boxShadow: '0 0 #0000, 0 0 #0000, 0 1px 2px 0 rgb(0 0 0 / 0.05)' }}
+        className="fixed bottom-[calc(4rem+1.5rem)] right-0 mr-4 bg-white p-6 rounded-lg border border-[#e5e7eb] w-[440px] h-[634px] flex flex-col"
       >
         {/* Heading */}
         <div className="flex flex-col space-y-1.5 pb-6">
           <h2 className="font-semibold text-lg tracking-tight">Chatbot</h2>
-          <p className="text-sm text-[#6b7280] leading-3">
-            Powered by Mendable and Vercel
-          </p>
+          <p className="text-sm text-[#6b7280] leading-3">Powered by Mendable and Vercel</p>
         </div>
 
         {/* Chat Container */}
         <div
-          className="pr-4 h-[474px]"
-          style={{ minWidth: "100%", display: "table" }}
+          className="flex-1 overflow-y-auto pr-4"
+          style={{ minWidth: '100%' }}
         >
-          {messages.map((msg, index) =>
-            msg.role === "system" ? (
-              <div
-                key={index}
-                className="flex gap-3 my-4 text-gray-600 text-sm flex-1"
-              >
-                <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
-                  <div className="rounded-full bg-gray-100 border p-1">
-                    <svg
-                      stroke="none"
-                      fill="black"
-                      strokeWidth="1.5"
-                      viewBox="0 0 24 24"
-                      aria-hidden="true"
-                      height="20"
-                      width="20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
-                      ></path>
-                    </svg>
-                  </div>
-                </span>
-                <p className="leading-relaxed">
-                  <span className="block font-bold text-gray-700">AI </span>
-                  {msg.content}
-                </p>
+          {/* Chat Message AI */}
+          <div className="flex gap-3 my-4 text-gray-600 text-sm">
+            <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
+              <div className="rounded-full bg-gray-100 border p-1">
+                <svg
+                  stroke="none"
+                  fill="black"
+                  strokeWidth="1.5"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                  height="20"
+                  width="20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+                  />
+                </svg>
               </div>
-            ) : (
-              <div
-                key={index}
-                className="flex gap-3 my-4 text-gray-600 text-sm flex-1"
-              >
-                <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
-                  <div className="rounded-full bg-gray-100 border p-1">
-                    <svg
-                      stroke="none"
-                      fill="black"
-                      strokeWidth="0"
-                      viewBox="0 0 16 16"
-                      height="20"
-                      width="20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"></path>
-                    </svg>
-                  </div>
-                </span>
-                <p className="leading-relaxed">
-                  <span className="block font-bold text-gray-700">You </span>{" "}
-                  {msg.content}
-                </p>
+            </span>
+            <p className="leading-relaxed">
+              <span className="block font-bold text-gray-700">AI </span>
+              Hola, ¿qué lugares turísticos de Encarnación te gustaría visitar?
+            </p>
+          </div>
+
+          {/* User Chat Message */}
+          <div className="flex gap-3 my-4 text-gray-600 text-sm">
+            <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
+              <div className="rounded-full bg-gray-100 border p-1">
+                <svg
+                  stroke="none"
+                  fill="black"
+                  strokeWidth="0"
+                  viewBox="0 0 16 16"
+                  height="20"
+                  width="20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"
+                  />
+                </svg>
               </div>
-            )
-          )}
+            </span>
+            <p className="leading-relaxed">
+              <span className="block font-bold text-gray-700">You </span>
+              Recomiéndame lugares bailables.
+            </p>
+          </div>
+
+          {/* AI Chat Message */}
+          <div className="flex gap-3 my-4 text-gray-600 text-sm">
+            <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
+              <div className="rounded-full bg-gray-100 border p-1">
+                <svg
+                  stroke="none"
+                  fill="black"
+                  strokeWidth="1.5"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                  height="20"
+                  width="20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+                  />
+                </svg>
+              </div>
+            </span>
+            <p className="leading-relaxed">
+              <span className="block font-bold text-gray-700">AI </span>
+              Claro, estos son los...
+            </p>
+          </div>
         </div>
 
         {/* Message Input */}
-        <div className="w-full flex items-center border-t py-2">
+        <div className="w-full flex items-center border-t py-2 bg-white border-t-[#e5e7eb]">
           <input
             type="text"
             placeholder="Type a message..."
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
             className="flex-1 border-none p-2 text-gray-700 focus:outline-none"
           />
-          <button
-            onClick={() => handleRequest(message)}
-            className="bg-black text-white px-4 py-2 rounded-full ml-2"
-          >
-            Send
-          </button>
+          <button className="bg-black text-white px-4 py-2 rounded-full ml-2">Send</button>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Chatbot;
